18735f1a8148fd6aedc1609b3309367cd6a02dce
baihe <baihe773373@126.com>
2018-08-09 14:34:29 +0800

    登录页面路由配置

---
+++ src/views/login.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <el-form :label-position="labelPosition" label-width="80px" :model="formLabelAlign">
+      <el-form-item label="用户名">
+        <el-input v-model="formLabelAlign.name"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input type="password" v-model="formLabelAlign.region"></el-input>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      labelPosition: 'right',
+      formLabelAlign: {
+        name: '',
+        region: '',
+        type: ''
+      }
+    };
+  }
+};
+</script>
+<style>
+
+</style>
