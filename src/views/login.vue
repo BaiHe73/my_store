2f5e8b1db9c649950d2f7814d22bdd4eb57721dd
baihe <baihe773373@126.com>
2018-08-09 21:31:18 +0800

    登录功能

--- src/views/login.vue
+++ src/views/login.vue
@@ -12,11 +12,14 @@
       <el-form-item label="密码">
         <el-input type="password" v-model="formData.password"></el-input>
       </el-form-item>
-      <el-button class="login-btn" type="primary">登录</el-button>
+      <el-button @click="handleLogin" class="login-btn" type="primary">登录</el-button>
     </el-form>
   </div>
 </template>
 <script>
+// 导入axios模块
+import axios from 'axios';
+
 export default {
   data() {
     return {
@@ -25,6 +28,38 @@ export default {
         password: ''
       }
     };
+  },
+  methods: {
+    handleLogin() {
+      axios
+        .post('http://localhost:8888/api/private/v1/login', this.formData)
+        .then((response) => {
+          // data:
+          //   data:
+          //   {id: 500, rid: 30, username: "admin", mobile: "12345678", email"adsfad@qq.com" }
+          //   meta:
+          //   {msg: "登录成功", status: 200}
+          // console.log(response);
+          var status = response.data.meta.status;
+          var message = response.data.meta.msg;
+          if (status === 200) {
+            // 登录成功
+            // 提示
+            this.$message.success(message);
+            // 获取token 记录在sessionStorage中
+            var token = response.data.data.token;
+            sessionStorage.setItem('token', token);
+            // console.log(token);
+            // 跳转
+          } else {
+            // 登录失败
+            this.$message.error(message);
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
   }
 };
 </script>
