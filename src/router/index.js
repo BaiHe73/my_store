14610c0f4bac8764e54ac5ac7a6c7a9da1aeba58
baihe <baihe773373@126.com>
2018-08-10 13:16:03 +0800

    首页路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 // 引入login模块
 import Login from '@/views/login';
+import Home from '@/views/home';
 // @ 代表的是src的绝对路径
 // @ 是在build/webpack.base.conf.js
 // 为什么可以省略.vue  在build/webpack.base.conf.js 中配置了
@@ -12,6 +13,7 @@ Vue.use(Router);
 
 export default new Router({
   routes: [
+    { name: 'Home', path: '/', component: Home },
     { name: 'Login', path: '/login', component: Login }
   ]
 });
