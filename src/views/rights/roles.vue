d8fa291f46227acbefca3586dc5df38d9a7fad4a
baihe <baihe773373@126.com>
2018-08-13 01:59:36 +0800

    角色权限的展开

--- src/views/rights/roles.vue
+++ src/views/rights/roles.vue
@@ -12,6 +12,48 @@
       stripe
       :data="rolesData"
       style="width: 100%; margin-top:10px">
+      <el-table-column type="expand">
+        <template slot-scope="scope">
+          <el-row
+            class="level1"
+            v-for="level1 in scope.row.children"
+            :key="level1.id">
+            <el-col :span="4">
+              <el-tag
+                closable>
+                {{level1.authName}}
+              </el-tag>
+            </el-col>
+            <el-col :span="20">
+              <el-row
+                class="level2"
+                v-for="level2 in level1.children"
+                :key="level2.id">
+                <el-col :span="4">
+                  <el-tag
+                    closable
+                    type="success">
+                    {{level2.authName}}
+                  </el-tag>
+                </el-col>
+                <el-col :span="20">
+                  <el-tag
+                    class="level3"
+                    v-for="level3 in level2.children"
+                    :key="level3.id"
+                    closable
+                    type="warning">
+                    {{level3.authName}}
+                  </el-tag>
+                </el-col>
+              </el-row>
+            </el-col>
+          </el-row>
+          <el-row v-if="scope.row.children.length === 0">
+            {{ '没有权限' }}
+          </el-row>
+        </template>
+      </el-table-column>
       <el-table-column
         type="index"
         width="50">
@@ -73,7 +115,7 @@ export default {
       const { meta: { status, msg } } = response.data;
       if (status === 200) {
         this.rolesData = response.data.data;
-        this.$message.success(msg);
+        // this.$message.success(msg);
       } else {
         this.$message.error(msg);
       }
@@ -83,5 +125,14 @@ export default {
 </script>
 
 <style>
-
+  .level1 {
+    margin-bottom: 10px;
+  }
+  .level2 {
+    margin-bottom: 6px;
+  }
+  .level3 {
+    margin-bottom: 4px;
+    margin-right: 4px;
+  }
 </style>
